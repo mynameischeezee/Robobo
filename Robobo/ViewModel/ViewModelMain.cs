55e59f7bc8d94309608e9042ca09826a3ca42e46
Макс <maksprofy51@gmail.com>
2020-05-20 20:28:04 +0300

    Add project files.

---
+++ Robobo/ViewModel/ViewModelMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Robobo.ViewModel
+{
+    class ViewModelMain : INotifyPropertyChanged
+    {
+
+    }
+}
